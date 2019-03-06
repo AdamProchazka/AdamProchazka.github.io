7774e11aef5e55ea9d0bda79506dd33ed683cb5f
Adam Procházka <sembatman@live.com>
2019-03-06 16:40:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -367,7 +367,7 @@ function signInfun(){
         'apiKey': API_KEY,
         'clientId': CLIENT_ID,
         'scope': SCOPE,
-        'discoveryDocs': ['http://sheets.googleapis.com/$discovery/rest?version=v4'],
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
       }).then(function() {
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
         updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
